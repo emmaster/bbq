8f6ebd32536e17ec61b4a7ccc9d4a410da8fce3a
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-07-29 17:02:25 +0300

    photos and avatars functional

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,4 +10,8 @@ class Event < ActiveRecord::Base
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
+
+  def visitors
+    (subscribers + [user]).uniq
+  end
 end
