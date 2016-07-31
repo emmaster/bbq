d73071347c522d13e1215a99475b7663a731221e
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-07-31 15:30:19 +0300

    everything for mailing

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -9,11 +9,12 @@ class SubscriptionsController < ApplicationController
     @new_subscription = @event.subscriptions.build(subscription_params)
     @new_subscription.user = current_user
 
-    if @new_subscription.user.present? && @event.user.email == @new_subscription.user.email
+    if @event.user == current_user
       redirect_to @event, alert: I18n.t('controllers.subscription.host_cant_subscribe') and return nil
     end
 
     if @new_subscription.save
+      EventMailer.subscription(@event, @new_subscription).deliver_now
       redirect_to @event, notice: I18n.t('controllers.subscription.created')
     else
       render 'events/show', alert: I18n.t('controllers.subscription.error')
