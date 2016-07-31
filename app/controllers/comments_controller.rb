c79d7e819daef81065c71b7217dc10f170ff2d0a
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-07-31 18:18:23 +0300

    no email to the comment's author

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -48,7 +48,10 @@ class CommentsController < ApplicationController
 
     def notify_subscribers(event, comment)
       #собираем всех подписчиков
+
       all_emails = (event.subscriptions.map(&:user_email)+ [event.user.email]).uniq
+
+      all_emails.delete(current_user.email) if current_user.present?
 
       all_emails.each do |mail|
         EventMailer.comment(event, comment, mail).deliver_now
